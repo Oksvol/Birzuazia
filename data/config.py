47da8e328f6a4262c159bd8d96d8c400d7701fdb
Oksvol <oksvolian@gmail.com>
2021-08-25 02:00:16 +0300

    Нормально поставил токены

--- data/config.py
+++ data/config.py
@@ -1,9 +1,12 @@
-from environs import Env
+# from environs import Env
+#
+# # Теперь используем вместо библиотеки python-dotenv библиотеку environs
+# env = Env()
+# env.read_env()
+#
+# BOT_TOKEN = env('BOT_TOKEN')  # Забираем значение типа str
+# ADMINS = env.list('ADMINS')  # Тут у нас будет список из админов
+# IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = '1962825445:AAFBA2ewKisVPDZNfgTnmav0X8pBll2ykQo'
+ADMINS = '112213378'
