de3f1cd6571137779e4a310731b015e2767e8d43
Oksvol <oksvolian@gmail.com>
2021-08-25 15:55:56 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -8,5 +8,5 @@
 # ADMINS = env.list('ADMINS')  # Тут у нас будет список из админов
 # IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
-BOT_TOKEN = '1962825445:AAFBA2ewKisVPDZNfgTnmav0X8pBll2ykQo'
-ADMINS = ['112213378']
+BOT_TOKEN = 
+ADMINS =
