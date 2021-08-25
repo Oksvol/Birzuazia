81f93a4a1899e294acdf3b95357fd7c5b5bed1ef
Oksvol <oksvolian@gmail.com>
2021-08-25 16:06:39 +0300

    Защита проекта

--- data/config.py
+++ data/config.py
@@ -1,12 +1,10 @@
-# from environs import Env
-#
-# # Теперь используем вместо библиотеки python-dotenv библиотеку environs
-# env = Env()
-# env.read_env()
-#
-# BOT_TOKEN = env('BOT_TOKEN')  # Забираем значение типа str
-# ADMINS = env.list('ADMINS')  # Тут у нас будет список из админов
-# IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+from environs import Env
 
-BOT_TOKEN = 
-ADMINS =
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env('BOT_TOKEN')  # Забираем значение типа str
+ADMINS = env.list('ADMINS')  # Тут у нас будет список из админов
+IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+
+
